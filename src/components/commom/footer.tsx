a1467e7ce00b405e289d85fb495606cbeeec121c
felipeneneu <feliperpg3@gmail.com>
2025-08-10 02:46:45 -0300

    feat: add footer

---
+++ src/components/commom/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="w-full gap-1 bg-accent p-8">
+      <p className="font-medium text-xs">© 2025 Copyright BEWEAR</p>
+      <p className="font-medium text-muted-foreground text-xs">
+        Todos os direitos reservados.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
